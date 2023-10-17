4c993915fef9a71c63f18fb8549d3400fb4b43e7
bautistaRenaudo <bautista.renaudo@ivelopment.com>
2023-10-17 12:06:05 -0300

    create basics services workers

---
+++ sw.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = 'mi-cache';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/index-fc1d2f65.css',
+    '/assets/index-a44ea685.js',
+    '/assets/Iconpin-22bb335a.svg',
+    '/assets/green-arrow-ff12fa96.svg',
+    '/manifest.json',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache abierto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = ['mi-cache'];
+
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
